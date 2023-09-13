512a87eeb78c2d97b45b0fde3d2aabd8a2586fb2
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2023-09-13 13:40:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# VariablesProject
+ Nscc Variables
---
+++ VariablesProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Variables");
+            Console.WriteLine("---------");
+            Console.ReadKey();
+        }
+    }
+}
