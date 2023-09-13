faf58f3f8e64c1fc4ac7925a22df57ada539a39d
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2023-09-13 13:44:29 -0300

    Added Score and Score Multiplier

--- VariablesProject/Program.cs
+++ VariablesProject/Program.cs
@@ -6,12 +6,18 @@ using System.Threading.Tasks;
 
 namespace VariablesProject
 {
+    //Score total
+    private int TotalScore
+    //Score Multiplier 
+    private int ScoreMult
+
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Variables");
             Console.WriteLine("---------");
+            Console.WriteLine("Press any key to Exit");
             Console.ReadKey();
         }
     }
