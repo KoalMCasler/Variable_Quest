48355f67e5ca9c61f8f545d287446fbbccd1bcf6
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2023-09-13 14:38:17 -0300

    played with variables

--- VariablesProject/Program.cs
+++ VariablesProject/Program.cs
@@ -6,17 +6,35 @@ using System.Threading.Tasks;
 
 namespace VariablesProject
 {
-    //Score total
-    private int TotalScore
-    //Score Multiplier 
-    private int ScoreMult
-
     internal class Program
     {
         static void Main(string[] args)
         {
+            string RealName;
+            string GamerTag;
+            RealName = "Koal Casler";
+            GamerTag = "TheeOBLIVION";
+            // Score Total
+            int TotalScore;
+            //Score Multiplier  
+            int ScoreMulti;
+            // Base increase in score.
+            int BaseScore;
+            float Standings;
+            float Standing;
+            float TotalPlayers;
+            ScoreMulti = 2;
+            BaseScore = 5;
+            Standing = 10f;
+            TotalPlayers = 153472f;
+            Standings = (Standing / TotalPlayers) * 100;
+            TotalScore = ScoreMulti * BaseScore;
             Console.WriteLine("Variables");
             Console.WriteLine("---------");
+            Console.WriteLine(RealName + " has the Gamer Tag of : " + GamerTag);
+            Console.WriteLine();
+            Console.WriteLine("Your Score = " + TotalScore);
+            Console.WriteLine("Your Standings are in the top : " + Standings + "% of all players");
             Console.WriteLine("Press any key to Exit");
             Console.ReadKey();
         }
