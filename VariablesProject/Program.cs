a70ea2120d784778b13adf8e8279f14d468e0aad
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2023-09-18 08:55:30 -0300

    Added Startup method

--- VariablesProject/Program.cs
+++ VariablesProject/Program.cs
@@ -8,35 +8,46 @@ namespace VariablesProject
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
             string RealName;
             string GamerTag;
             RealName = "Koal Casler";
-            GamerTag = "TheeOBLIVION";
+            StudioName = "Shrouded Fortress Entertainment";
             // Score Total
             int TotalScore;
             //Score Multiplier  
             int ScoreMulti;
             // Base increase in score.
             int BaseScore;
-            float Standings;
-            float Standing;
-            float TotalPlayers;
+            int MaxHp;
+            int CurrentHP;
+            int PlayerDam;
+            int EnemyDam;
+            int EnemyHp;
+        static void Main(string[] args)
+        {
+            Startup();
+            Console.WriteLine("Variable Quest!");
+            Console.WriteLine("By: " + RealName + " Of, " StudioName);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Your Score = " + TotalScore);
+            Console.WriteLine("Your Hp = " + CurrentHP);
+            Console.WriteLine("Press any key to Continue.");
+            Console.ReadKey();
+        }
+        void Startup()
+        {
+            EnemyHp = 15;
+            MaxHp = 20;
+            CurrentHP = MaxHp;
+            PlayerDam = 5;
+            EnemyDam = 2;
             ScoreMulti = 2;
             BaseScore = 5;
             Standing = 10f;
             TotalPlayers = 153472f;
             Standings = (Standing / TotalPlayers) * 100;
             TotalScore = ScoreMulti * BaseScore;
-            Console.WriteLine("Variables");
-            Console.WriteLine("---------");
-            Console.WriteLine(RealName + " has the Gamer Tag of : " + GamerTag);
-            Console.WriteLine();
-            Console.WriteLine("Your Score = " + TotalScore);
-            Console.WriteLine("Your Standings are in the top : " + Standings + "% of all players");
-            Console.WriteLine("Press any key to Exit");
-            Console.ReadKey();
         }
     }
 }
