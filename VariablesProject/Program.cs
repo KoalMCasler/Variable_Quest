0ffc3074808d8f6fad64cf3175c047f2524d9765
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2023-09-18 10:06:55 -0300

    Built and Finished the game!

--- VariablesProject/Program.cs
+++ VariablesProject/Program.cs
@@ -8,46 +8,138 @@ namespace VariablesProject
 {
     internal class Program
     {
-            string RealName;
-            string GamerTag;
-            RealName = "Koal Casler";
-            StudioName = "Shrouded Fortress Entertainment";
+            static string RealName;
+            static string StudioName;
             // Score Total
-            int TotalScore;
+            static int TotalScore;
             //Score Multiplier  
-            int ScoreMulti;
+            static int ScoreMulti;
             // Base increase in score.
-            int BaseScore;
-            int MaxHp;
-            int CurrentHP;
-            int PlayerDam;
-            int EnemyDam;
-            int EnemyHp;
+            static int BaseScore;
+            static int MaxHp;
+            static int CurrentHP;
+            static int PlayerDam;
+            static int EnemyDam;
+            static int EnemyHp;
+            static int EnemyMaxHp;
+            static int PlayerLives;
+            static int KillCount;
+            static bool GameIsOver;
+
         static void Main(string[] args)
         {
             Startup();
             Console.WriteLine("Variable Quest!");
-            Console.WriteLine("By: " + RealName + " Of, " StudioName);
+            Console.WriteLine("By: " + RealName + " Of " + StudioName);
+            Console.WriteLine("*************************");
             Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Your Score = " + TotalScore);
-            Console.WriteLine("Your Hp = " + CurrentHP);
+            HUD();
             Console.WriteLine("Press any key to Continue.");
+            Console.WriteLine();
             Console.ReadKey();
+            Console.WriteLine();
+            Encounter();
+            while (CurrentHP > 0)
+                {
+                    Console.WriteLine("Press a Key to attack!");
+                    Console.ReadKey();
+                    Combat();
+                    HUD();
+                    if (CurrentHP <= 0)
+                    {
+                        Death();
+                        if (PlayerLives > 0)
+                        {
+                            GameLose();
+                        }
+                    }
+                    if (KillCount > 2)
+                    {
+                        GameWin();
+                    }
+                    if (GameIsOver == true)
+                    {
+                        Environment.Exit(0);
+                    }
+                }
         }
-        void Startup()
+        static void Startup()
         {
-            EnemyHp = 15;
+            PlayerLives = 3;
+            EnemyMaxHp = 15;
+            EnemyHp = EnemyMaxHp;
             MaxHp = 20;
             CurrentHP = MaxHp;
             PlayerDam = 5;
             EnemyDam = 2;
             ScoreMulti = 2;
-            BaseScore = 5;
-            Standing = 10f;
-            TotalPlayers = 153472f;
-            Standings = (Standing / TotalPlayers) * 100;
+            BaseScore = 100;
+            RealName = "Koal Casler";
+            StudioName = "Shrouded Fortress Entertainment";
+            GameIsOver = false;
+        }
+        static void Combat()
+        {
+            CurrentHP = CurrentHP - EnemyDam;
+            EnemyHp = EnemyHp - PlayerDam;
+            Console.WriteLine("The Enemy has " + EnemyHp + "HP left.");
+            if (EnemyHp <= 0)
+            {
+                Console.WriteLine("You killed the enemy!");
+                EnemyHp = EnemyMaxHp;
+                ScoreCalc();
+                KillCount += 1;
+            }
+            
+        }
+        static void HUD()
+        {
+            Console.WriteLine("**********");
+            Console.WriteLine("Your Score = " + TotalScore);
+            Console.WriteLine("Your Hp = " + CurrentHP);
+            Console.WriteLine("Your Lives = " + PlayerLives);
+            Console.WriteLine("Your Kills = " + KillCount);
+            Console.WriteLine("**********");
+        }
+        static void ScoreCalc()
+        {
             TotalScore = ScoreMulti * BaseScore;
+        }
+        static void GameEnd()
+        {
+            GameIsOver = true;
+        }
+        static void Death()
+        {
+            PlayerLives -= 1;
+            Console.WriteLine("You Died!");
+            HUD();
+            Console.WriteLine("Press any key to try again.");
+            Console.ReadKey();
+            CurrentHP = MaxHp;
+        }
+        static void Encounter()
+        {
+            Console.WriteLine("You encounter a wild Float!");
+            Console.WriteLine("The enemy Float has " + EnemyHp + " Total HP!");
+        }
+        static void GameWin()
+        {
+            Console.WriteLine("You Win!");
+            HUD();
+            Console.WriteLine("Press any key to Exit");
+            Console.ReadKey();
+            Console.WriteLine();
+            GameEnd();
+        }
+        static void GameLose()
+        {
+            Console.WriteLine("You Lose!");
+            HUD();
+            Console.WriteLine("Press any key to Exit");
+            Console.ReadKey();
+            Console.WriteLine();
+            GameEnd();
         }
     }
 }
