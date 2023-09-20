1a41f72227ec2e399a4be53d0d6ead3f7027bf09
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2023-09-20 15:25:47 -0300

    Commented the crap out of the code.

--- VariablesProject/Program.cs
+++ VariablesProject/Program.cs
@@ -8,7 +8,9 @@ namespace VariablesProject
 {
     internal class Program
     {
+            // My name
             static string RealName;
+            // Name of my studio
             static string StudioName;
             // Score Total
             static int TotalScore;
@@ -16,22 +18,36 @@ namespace VariablesProject
             static int ScoreMulti;
             // Base increase in score.
             static int BaseScore;
+            // Max Hit points. 
             static int MaxHp;
+            // Current HP
             static int CurrentHP;
+            // Player base damage
             static int PlayerDam;
+            // Enemy base damage
             static int EnemyDam;
+            // Enemy Current HP
             static int EnemyHp;
+            // Enemy max hp
             static int EnemyMaxHp;
+            // Player lives 
             static int PlayerLives;
+            // Player kill count
             static int KillCount;
+            // Game over trigger bool
             static bool GameIsOver;
+            // Name of enemy pulled from list
             static string EnemyName;
+            // List of Enemy names
             static List<string> EnemyList;
+            // Index for enemy list to pull one at random.
             static int ListIndex;
+            // Fancy line breaks
             static string StarLine;
 
         static void Main()
         {
+            // Game startup 
             Startup();
             Console.WriteLine("Variable Quest!");
             Credit(RealName,StudioName);
@@ -43,8 +59,10 @@ namespace VariablesProject
             Console.WriteLine();
             Console.WriteLine();
             Encounter();
+            // Main game play loop 
             while (CurrentHP > 0)
                 {
+                    // Ability to leave game.
                     if (Console.ReadKey().Key == ConsoleKey.Backspace)
                     {
                         GameEnd();
@@ -53,6 +71,7 @@ namespace VariablesProject
                     Console.ReadKey();
                     Combat();
                     HUD();
+                    // Player death trigger
                     if (CurrentHP <= 0)
                     {
                         Death();
@@ -61,14 +80,17 @@ namespace VariablesProject
                             GameLose();
                         }
                     }
+                    // Win condition
                     if (KillCount >= 5)
                     {
                         GameWin();
                     }
+                    // Game ender
                     if (GameIsOver == true)
                     {
                         Environment.Exit(0);
                     }
+                    // heal ability
                     Console.WriteLine("\nPress E to Heal back to full HP.\n");
                     if (Console.ReadKey().Key == ConsoleKey.E)
                     {
@@ -79,6 +101,7 @@ namespace VariablesProject
         }
         static void Startup()
         {
+            // The instillation 
             PlayerLives = 3;
             EnemyMaxHp = 15;
             EnemyHp = EnemyMaxHp;
@@ -96,6 +119,7 @@ namespace VariablesProject
         }
         static void Combat()
         {
+            // Combat function
             CurrentHP -=  EnemyDam;
             EnemyHp -=  PlayerDam;
             Console.WriteLine("The Enemy" + EnemyName + " has " + EnemyHp + "HP left.");
@@ -112,6 +136,7 @@ namespace VariablesProject
         }
         static void HUD()
         {
+            // prints the "HUD"
             Console.WriteLine(StarLine);
             Console.WriteLine("Your Score = " + TotalScore);
             Console.WriteLine("Your Hp = " + CurrentHP);
@@ -122,14 +147,17 @@ namespace VariablesProject
         }
         static void ScoreCalc()
         {
+            // Calculates score
             TotalScore += ScoreMulti * BaseScore;
         }
         static void GameEnd()
         {
+            // Sets game end bool to true and shuts off program
             GameIsOver = true;
         }
         static void Death()
         {
+            // lowers player lives, and lets you try again. 
             PlayerLives -= 1;
             Console.WriteLine("You Died!");
             Console.WriteLine("Press any key to try again.");
@@ -138,12 +166,14 @@ namespace VariablesProject
         }
         static void Encounter()
         {
+            // Selects a random enemy name
             EnemyRandomizer();
             Console.WriteLine("You encounter a wild" + EnemyName + "!");
             Console.WriteLine("The enemy" + EnemyName + " has " + EnemyHp + " Total HP!");
         }
         static void GameWin()
         {
+            // Displays win text and credits when game is won!
             HUD();
             Console.WriteLine("You Win!");
             Console.WriteLine("Press any key to Exit");
@@ -153,6 +183,7 @@ namespace VariablesProject
         }
         static void GameLose()
         {
+            // Displays lose text and ends game.
             Console.WriteLine("You Lose!");
             Console.WriteLine("Press any key to Exit");
             Console.ReadKey();
@@ -160,14 +191,17 @@ namespace VariablesProject
         }
         static void Heal()
         {
+            // heals player to max HP
             CurrentHP = MaxHp;
         }
         static void Credit(string PName, string SName)
         {
+            // Writes Credit String
             Console.WriteLine("By: " + PName + " Of " + SName);
         }
         static void EnemyRandomizer()
         {
+            // Pulls random enemy from list of enemies
             Random rnd = new Random();
             ListIndex = rnd.Next(3);
             EnemyName = EnemyList.ElementAt(ListIndex);
